0f4c29268e0cf2fcf38f390aca572bc81f738d3e
nicodemossjunior <nicodemossjunior@gmail.com>
2024-11-28 15:04:15 -0300

    Fixing implementation to chatgpt API access

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -1,46 +1,23 @@
-//import OpenAI from "openai";
-//const { Configuration, OpenAIApi } = require("openai")
 const { OpenAI } = require("openai")
-
 
 module.exports = class openai{
 
     static configuration(){
-        // const configuration = new Configuration({
-        //     apiKey: process.env.OPENAI_API_KEY,
-        // })
-
         return new OpenAI({
             apiKey: process.env.OPENAI_API_KEY
         });
     }
 
     static async textCompletion({prompt}){
-
-
+        console.log(prompt)
         return {
-            model: "gpt-3.5-turbo",
-            messages: [
-                { role: "user", content: `${prompt}` },
-                { role: "assistant", content: "" }, 
-            ],
-            temperature: 0,// Controla a "criatividade" da resposta
-            max_tokens: 3500,// Limite de tokens
-            top_p: 1,// Amostragem
-        }
+                    model: "gpt-3.5-turbo",
+                    messages: [
+                        { "role": "user", "content": `${prompt}` },
+                        { "role": "assistant", "content": "" }, 
+                    ],
+                    temperature: 1,
+                    max_tokens: 2048,
+                }
     }
 }
-/*
-  return{
-        model: "text-davinci-003",
-        prompt: `${prompt}`,
-        temperature: 0,
-        max_tokens: 3500,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-    }
- */
-
-//model: "gpt-4o-mini"
-//gpt-3.5-turbo
--- server/src/controllers/prompt-controller.js
+++ server/src/controllers/prompt-controller.js
@@ -4,35 +4,21 @@ const openai = require("../config/openai")
 module.exports = {
     async sendText(req, res) {
 
-        console.log("-- inicio sendText --");
-        // console.log(req.body);
-        
+        console.log("-- inicio sendText --");       
 
         const openaiAPI = openai.configuration()
+
         const inputModel = new inputPrompt(req.body)
         console.log(inputModel)
 
         try {
-            //const response = await openaiAPI.createChatCompletion(
-            const response = await openaiAPI.chat.completions.create(
-                //openai.textCompletion(inputModel)
-                {
-                    model: "gpt-3.5-turbo",
-                    messages: [
-                        { role: "user", content: `${inputModel}` },
-                        { role: "assistant", content: "" }, 
-                    ],
-                    temperature: 0,// Controla a "criatividade" da resposta
-                    max_tokens: 3500,// Limite de tokens
-                    top_p: 1,// Amostragem
-                }
-            )
-
-            console.log(JSON.stringify(response))
+            const response = await openaiAPI.chat.completions.create(await openai.textCompletion(inputModel))
+            
+            console.log("Response: "+response.choices[0].message.content)
 
             return res.status(200).json({
                 sucess:true,
-                data: response.data.choices[0].message
+                data: response.choices[0].message.content
             })
         } catch (error) {
             console.log(error)
